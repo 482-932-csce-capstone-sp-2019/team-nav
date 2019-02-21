65031bb9d45164fa1b6f2b79fc682df6f4cea556
Suraj Swarnapuri <thriples1@gmail.com>
2019-02-21 08:35:27 -0600

    Add project files.

---
+++ ArcGISAppDemo/ArcGISAppDemo.Shared/MapPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿
+using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Xamarin.Forms;
+
+
+namespace ArcGISAppDemo
+{
+    public partial class ChangeBasemap : ContentPage
+    {
+        public string getJson(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private readonly Dictionary<string, Basemap> _basemapOptions = new Dictionary<string, Basemap>()
+        {
+            {"Streets (Raster)", Basemap.CreateStreets()},
+            {"Streets (Vector)", Basemap.CreateStreetsVector()},
+            {"Streets - Night (Vector)", Basemap.CreateStreetsNightVector()},
+            {"Imagery (Raster)", Basemap.CreateImagery()},
+            {"Imagery with Labels (Raster)", Basemap.CreateImageryWithLabels()},
+            {"Imagery with Labels (Vector)", Basemap.CreateImageryWithLabelsVector()},
+            {"Dark Gray Canvas (Vector)", Basemap.CreateDarkGrayCanvasVector()},
+            {"Light Gray Canvas (Raster)", Basemap.CreateLightGrayCanvas()},
+            {"Light Gray Canvas (Vector)", Basemap.CreateLightGrayCanvasVector()},
+            {"Navigation (Vector)", Basemap.CreateNavigationVector()},
+            {"OpenStreetMap (Raster)", Basemap.CreateOpenStreetMap()}
+        };
+
+        public ChangeBasemap()
+        {
+            InitializeComponent();
+
+            Title = "Change basemap";
+
+            // Create the UI, setup the control references and execute initialization
+            Initialize();
+        }
+
+        private async void OnChangeBasemapButtonClicked(object sender, EventArgs e)
+        {
+            // Show sheet and get title from the selection
+            var selectedBasemap =
+                await DisplayActionSheet("Select basemap", "Cancel", null, _basemapOptions.Keys.ToArray());
+
+            // Retrieve the basemap from the dictionary
+            MyMapView.Map.Basemap = _basemapOptions[selectedBasemap];
+        }
+
+        private void Initialize()
+        {
+            // Create new Map with basemap
+            Map myMap = new Map(Basemap.CreateTopographic());
+
+            // Assign the map to the MapView
+            MyMapView.Map = myMap;
+        }
+    }
+}
